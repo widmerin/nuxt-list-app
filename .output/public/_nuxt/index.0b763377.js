40a6afa1e00dc5232f586429315ba2fe98be906d
Ina Widmer <widmerina@gmail.com>
2023-05-01 18:23:55 +0200

    remove .output folder from git

--- .output/public/_nuxt/index.0b763377.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as o,o as t,e as c}from"./entry.a0c40ef6.js";const n={},s=c("h1",null,"hello",-1),a=[s];function r(_,l){return t(),o("div",null,a)}const i=e(n,[["render",r]]);export{i as default};
